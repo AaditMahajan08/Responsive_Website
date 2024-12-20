acdea62efb876371f11086f32d250a492d75641b
AaditMahajan08 <aaditmahajan08@gmail.com>
2024-12-20 22:09:46 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Responsive_Website/',
+  base: '/Responsive_Website/', // Ensure this matches your repository name
   plugins: [react()]
 });
